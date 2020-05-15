6fa9c911696f2e88f262d1755631ae24698ff358
jms1132 <jms1132@naver.com>
2020-05-15 18:22:56 +0900

    Modals, forms and validation finish

--- awesome-todo/quasar.conf.js
+++ awesome-todo/quasar.conf.js
@@ -41,11 +41,23 @@ module.exports = function (ctx) {
         'QTabs',
         'QTab',
         'QRouteTab',
-        'QCheckbox'
+        'QCheckbox',
+        'QDialog',
+        'QCard',
+        'QCardSection',
+        'QCardActions',
+        'QSpace',
+        'QInput',
+        'QDate',
+        'QPopupProxy',
+        'QTime'
+
+
       ],
 
       directives: [
-        'Ripple'
+        'Ripple',
+        'ClosePopup'
       ],
 
       // Quasar plugins
