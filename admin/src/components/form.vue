880db0ed003cb9e00e4028356930ac2fe34e17d1
jms1132 <jms1132@naver.com>
2020-06-15 14:18:14 +0900

    component making

--- admin/src/components/form.vue
+++ admin/src/components/form.vue
@@ -1,11 +1,215 @@
-<template>
-	
+<template >
+	<q-form @submit="onSubmit">
+
+		<div class="qna_write_div">
+			<p class="q-mr-lg inline-block">{{ $t('form.title') }}</p>
+			<q-input
+				v-if="form_type == 'show'"
+				style="width:80%"
+				class="inline-block"
+				outlined
+				disable
+				v-model="title"
+			/>
+			<q-input
+				v-else
+				style="width:80%"
+				class="inline-block"
+				outlined
+				v-model="title"
+			/>
+		</div>
+		<!-- 내용 div -->
+		<div>
+			<p class="q-mr-lg inline-block q-mt-xl"> {{ $t('form.content')}}</p>
+			<q-input
+				v-if="form_type == 'show'"
+				style="width:80%"
+				class="inline-block"
+				outlined
+				disable
+				type="textarea"
+				v-model="contents"
+			/>
+			<q-input
+				v-else
+				class="inline-block qna_contents"
+				outlined
+				type="textarea"
+				style="width:80%"
+				v-model="contents"
+			/>
+		</div>
+
+		<div>
+			<div class="flex flex-center column">
+				<div
+					class="row"
+					style="width: 71%; "
+				>
+					<div
+						class="fit row wrap justify-end items-start content-start"
+						style="overflow: hidden;"
+					>
+						<div style="overflow: auto;">
+							<q-card>
+								<q-card-section v-if="form_type == 'show'">
+									<q-btn
+										:label="$t('buttons.modify')"
+										class="q-mr-sm"
+										@click="goEdit"
+									></q-btn>
+									<q-btn
+										:label="$t('buttons.delete')"
+										class="q-mr-sm"
+										@click="confirm = true"
+									></q-btn>
+									<q-btn
+										class="q-ma-none"
+										:label="$t('buttons.list')"
+										:to="nav.to"
+									></q-btn>
+								</q-card-section>
+
+								<q-card-section v-else>
+									<q-btn
+										:label="$t('buttons.register')"
+										class="q-mr-sm"
+										type="submit"
+									></q-btn>
+									<q-btn
+										class="q-ml-sm"
+										:label="$t('buttons.list')"
+										:to="nav.to"
+									></q-btn>
+
+								</q-card-section>
+							</q-card>
+						</div>
+					</div>
+				</div>
+			</div>
+			<q-dialog
+				v-model="confirm"
+				persistent
+			>
+				<q-card>
+					<q-card-section class="row items-center">
+						<q-avatar
+							icon="delete"
+							color="primary"
+							text-color="white"
+						/>
+						<span class="q-ma-lg">{{$t('confirm.delete_ask')}}</span>
+					</q-card-section>
+
+					<q-card-actions align="right">
+						<q-btn
+							flat
+							:label="$t('buttons.cancel')"
+							color="primary"
+							v-close-popup
+						/>
+						<q-btn
+							class="q-ml-sm"
+							flat
+							:label="$t('buttons.delete')"
+							color="primary"
+							v-close-popup
+							@click="deleteData"
+						/>
+					</q-card-actions>
+				</q-card>
+			</q-dialog>
+		</div>
+
+	</q-form>
 </template>
 <script>
+import axios from 'axios'
+
 export default {
-	
+	data() {
+		return {
+			title: null,
+			contents: null,
+			type: 'notice',
+			user_id: '1',
+			confirm: false,
+			nav: {
+				to: '/notice_list'
+			},
+			form_type: this.$route.query.form_type,
+			postNumber: this.$route.query.id
+		}
+	},
+	created() {
+		if (this.form_type == 'edit' || this.form_type == 'show') {
+			axios
+				.get('http://localhost:8000/api/v1/admin/article/' + this.postNumber)
+				.then(response => {
+					this.title = response.data.data.title
+					this.contents = response.data.data.contents
+
+					console.log(this.title)
+					console.log(this.contents)
+
+					console.log(this.postNumber)
+					console.log(this.form_type)
+				})
+				.catch(error => {
+					console.log(error)
+				})
+		} else if (this.form_type == 'register') {
+			console.log(this.form_type)
+		} else if (this.form_type == 'show') {
+			console.log(this.form_type)
+		}
+	},
+
+	methods: {
+		goEdit() {
+			this.$router.push({
+				name: 'notice_edit',
+				query: { id: this.postNumber, form_type: 'edit' }
+			})
+		},
+		deleteData() {
+			axios
+				.delete('http://localhost:8000/api/v1/admin/article/' + this.postNumber)
+				.then(response => {
+					console.log(this.$route.query.id)
+
+					this.$router.push('/notice_list')
+				})
+		},
+		onSubmit() {
+			axios({
+				method: this.form_type == 'register' ? 'post' : 'put',
+				url:
+					this.form_type == 'register'
+						? 'http://localhost:8000/api/v1/admin/article'
+						: 'http://localhost:8000/api/v1/admin/article/' + this.postNumber,
+				data: {
+					title: this.title,
+					contents: this.contents,
+					type: 'notice',
+					user_id: '1'
+				}
+			})
+				.then(response => {
+					this.$router.push('/notice_list')
+				})
+				.catch(error => {
+					console.log(error)
+					console.log('실패')
+				})
+		}
+	}
 }
 </script>
-<style>
-
+<style >
+textarea {
+	height: 400px;
+}
 </style>
--- admin/src/pages/PageNotice_list.vue
+++ admin/src/pages/PageNotice_list.vue
@@ -66,13 +66,8 @@
 					:to-fn="page=>({query:{page: page}})"
 				/>
 			</div>
-
 		</div>
 
-		<!--
-  Forked from:
-  https://quasar.dev/layout/grid/flex-playground#flex-example
--->
 		<div id="q-app">
 			<div class="flex flex-center column">
 
@@ -88,10 +83,12 @@
 						<div style="overflow: auto;">
 							<q-card>
 								<q-card-section class="q-pt-none">
-									<q-btn :to="nav.to">{{ $t('buttons.write')}}</q-btn>
+									<q-btn
+									
+										@click="register_post"
+									>{{ $t('buttons.write')}}</q-btn>
 								</q-card-section>
 							</q-card>
-
 						</div>
 					</div>
 				</div>
@@ -112,21 +109,11 @@ export default {
 			postData: [],
 			perPage: null,
 			currentPage: null,
-		
 			keyword: null,
 			total: null,
 			page: null,
-
 			test: null,
-
-			pagination: {
-				// sortBy: 'id',
-				// descending: false,
-				// page: 1,
-				// rowsNumber: 10
-				//이건 내가 만든 페이지네이션 자체에서 한페이지에 표현되는 갯수를 정한것이다. 이것과 서버의 perPage를 맞추고 나서 또 각각의 페이지에 대한 자료를 따로 요청해야겠지?
-			},
-
+			pagination: {},
 			columns: [
 				{
 					name: 'id',
@@ -151,17 +138,14 @@ export default {
 			}
 		}
 	},
-
 	created() {
-		
-
-		this.pagination= {
-				sortBy: 'id',
-				descending: false,
-				page: this.currentPage,
-				rowsNumber: this.perPage
-				//이건 내가 만든 페이지네이션 자체에서 한페이지에 표현되는 갯수를 정한것이다. 이것과 서버의 perPage를 맞추고 나서 또 각각의 페이지에 대한 자료를 따로 요청해야겠지?
-			},
+		(this.pagination = {
+			sortBy: 'id',
+			descending: true,
+			page: this.currentPage,
+			rowsNumber: this.perPage
+			//이건 내가 만든 페이지네이션 자체에서 한페이지에 표현되는 갯수를 정한것이다. 이것과 서버의 perPage를 맞추고 나서 또 각각의 페이지에 대한 자료를 따로 요청해야겠지?
+		}),
 			this.fetchData()
 	},
 	watch: {
@@ -170,46 +154,36 @@ export default {
 	computed: {
 		pagesNumber() {
 			//전체 나올 페이지의 수
-
 			return Math.ceil(this.total / this.perPage) //Math.ceil은 소수값이 존재할 때 갑을 올리는 역할을 하는 함수
 		}
 	},
 	methods: {
 		fetchData() {
 			this.pagination.page = this.$route.query.page
-				axios
-					.get('http://localhost:8000/api/v1/admin/article', {
-						params: {
-							type: 'notice',
-							page: this.pagination.page
-						}
-					})
-					.then(response => {
-						console.log(response)
-						this.postData = response.data.data.data
-
-						this.perPage = response.data.data.per_page //서버에서는 perPage를 한페이지에 담을 게시글수를 정해서 전달한다. 기본이 1페이지고 서버에서 10이라고 정해둿다면 화면에 10개만 전달된다.
-						//2페이지를 요청하면 그 다음 10개가 전달되겠지? 이거는 그냥 정말 서버에서 전달되는 갯수이다.
-
-						this.currentPage = response.data.data.current_page
-						this.total = response.data.data.total
-						console.log(this.clickPage + '번 페이지 클릭')
-					})
-					.catch(error => {
-						console.log(error)
-					})
+			axios
+				.get('http://localhost:8000/api/v1/admin/article', {
+					params: {
+						type: 'notice',
+						page: this.pagination.page
+					}
+				})
+				.then(response => {
+					console.log(response)
+					this.postData = response.data.data.data
+					
+					this.perPage = response.data.data.per_page
+					this.currentPage = response.data.data.current_page
+					console.log(this.currentPage)
+					this.total = response.data.data.total
+				})
+				.catch(error => {
+					console.log(error)
+				})
 		},
 		onRowClick(evt, row) {
-			// console.log('clicked on', row.id)
-
-			// this.$router.push('/notice_show')
-			// this.$router.push({
-			// 	name: 'notice_show',
-			// 	params: { id: row.id }
-			// })
 			this.$router.push({
 				name: 'notice_show',
-				query: { id: row.id }
+				query: { id: row.id, form_type : 'show' }
 			})
 			console.log(row.id)
 		},
@@ -229,6 +203,13 @@ export default {
 					console.log(error)
 					console.log('실패')
 				})
+		},
+		register_post() {
+			this.$router.push({
+				name: 'notice_form',
+				query: { form_type: 'register' }
+			})
+		
 		}
 	}
 }
